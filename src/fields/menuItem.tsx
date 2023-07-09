7cffe02ecc53d28ad5b0ca67ecc0ed41241307af
Chai Landau <chai.landau@gmail.com>
2023-07-09 19:17:16 -0400

    feat: Add nested submenu items to header

--- src/fields/menuItem.tsx
+++ src/fields/menuItem.tsx
@@ -5,27 +5,41 @@ import { capitalizeWord } from '../utils/functions';
 
 import { link } from './link';
 
+const getLabel = (defaultTitle: string) => ({
+    RowLabel: ({ data, index }) => {
+        const fallbackTitle = `${defaultTitle} ${String(index).padStart(2, '0')}`;
+
+        return (
+            <>
+                <strong>{data?.label || fallbackTitle}</strong>
+                &nbsp;|&nbsp;
+                {capitalizeWord(data?.linkType)}
+            </>
+        );
+    },
+});
+
 export const menuItem = [
     {
         name: 'menuItems',
         type: 'array',
-        fields: [
-            ...link
-        ],
         admin: {
-            components: {
-                RowLabel: ({ data, index }) => {
-                    const fallbackTitle = `Menu Item ${String(index).padStart(2, '0')}`;
-
-                    return (
-                        <>
-                            <strong>{data?.label || fallbackTitle}</strong>
-                            &nbsp;|&nbsp;
-                            {capitalizeWord(data?.linkType)}
-                        </>
-                    );
-                },
+            components: getLabel('Menu Item')
+        },
+        fields: [
+            ...link,
+            {
+                name: 'submenuItems',
+                type: 'array',
+                fields: [
+                    ...link
+                ],
+                admin: {
+                    description: 'Add submenu items here to create a dropdown menu.',
+                    components: getLabel('Submenu Item')
+                }
             }
-        }
+        ],
+
     },
 ] as Field[];
