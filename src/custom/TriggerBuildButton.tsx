b285c7ab091ead203eaaca948f0c0ef62b40376b
Chai Landau <chai.landau@gmail.com>
2023-10-27 17:56:30 -0400

    feat: add dynamic trigger build text * style: add styling * chore: remove console.log

--- src/custom/TriggerBuildButton.tsx
+++ src/custom/TriggerBuildButton.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { triggerBuildHook } from '../hooks/triggerBuild';
 
-const TriggerBuildButton = () => (
-    <button className='trigger-build-btn' type='button' onClick={triggerBuildHook}>Trigger Build</button>
-  );
-
-export default TriggerBuildButton;
+const TriggerBuildButton = () => {
+  
+  const [building, setBuilding] = useState(false);
+  
+  const buttonText = building ? 'Building...' : 'Trigger Me';
+  
+  const handleClick = () => {
+    triggerBuildHook();
+    setBuilding(true);
+    
+    setTimeout(() => {
+      setBuilding(false);
+    }, 60000);
+  };
+  
+  return (
+    <div className='trigger-build'>
+      <button className='trigger-build__btn' type='button' onClick={handleClick} disabled={building}>{buttonText}</button>
+      {building && (
+         <p className='trigger-build__text'>
+         <i>Build #triggered. Refresh site in 3-5 minutes. If nothing changes after 10 minutes, text Chai :D</i>
+       </p>
+      )}
+     
+    </div>
+    );};
+    
+    export default TriggerBuildButton;
+    
--- src/hooks/triggerBuild.ts
+++ src/hooks/triggerBuild.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 export const triggerBuildHook = async () => {
-  try {
-    console.log('hi5!');
-    
-return await axios.post(`${process.env.PAYLOAD_PUBLIC_CAPROVER_WEBHOOK}`);
+  try {    
+    return await axios.post(`${process.env.PAYLOAD_PUBLIC_CAPROVER_WEBHOOK}`);
   } catch (error) {
     console.error('Error:', error);
   }
