a16ecb13b5830a253470d908495c56e5b3df8724
Chai Landau <chai.landau@gmail.com>
2023-10-27 16:26:44 -0400

    fix: fix dotenv

--- src/hooks/triggerBuild.ts
+++ src/hooks/triggerBuild.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const triggerBuildHook = async () => {
   try {
-    return await axios.post(`${process.env.PAYLOAD_PUBLIC_CAPROVER_WEBHOOK}`);
+    console.log('hi!');
+    
+return await axios.post(`${process.env.PAYLOAD_PUBLIC_CAPROVER_WEBHOOK}`);
   } catch (error) {
     console.error('Error:', error);
   }
