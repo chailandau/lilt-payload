22ed9a018db827638f2c65f3310c39973047af7b
Chai Landau <chai.landau@gmail.com>
2023-10-27 14:55:44 -0400

    fix: remove unecessary dotenv

--- src/hooks/triggerBuild.ts
+++ src/hooks/triggerBuild.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv');
-
 export const triggerBuildHook = async () => {
   try {
     const response = await axios.post(`${process.env.PAYLOAD_PUBLIC_CAPROVER_WEBHOOK}`,
