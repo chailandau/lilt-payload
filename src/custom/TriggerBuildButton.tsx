c65b503a56813c0ddf45abf220cbf6b45f4fa999
Chai Landau <112015853+chailandau@users.noreply.github.com>
2023-10-27 16:04:18 -0400

    style: style trigger build button (#13)
    * fix: add dotenv to config

--- src/custom/TriggerBuildButton.tsx
+++ src/custom/TriggerBuildButton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { triggerBuildHook } from '../hooks/triggerBuild';
 
 const TriggerBuildButton = () => (
-    <button type='button' onClick={triggerBuildHook}>Trigger Build</button>
+    <button className='trigger-build-btn' type='button' onClick={triggerBuildHook}>Trigger Build</button>
   );
 
 export default TriggerBuildButton;
